0909ea7e6ceab97aebf767b3281ce8edde4f99ba
lucas__almeida <lucaslap27@gmail.com>
2021-09-08 13:00:00 -0400

    muda forma de pesquisa

--- start.js
+++ start.js
@@ -1,20 +1,55 @@
 const express = require("express");
 const app = express();
 const puppetter = require("puppeteer");
+const cheerio = require("cheerio");
+const axios = require("axios");
 const fs = require("fs");
 
 let url = "https://www.linkcorreios.com.br";
-let path = require("path");
 
 const server = require("http").Server(app);
 const cors = require("cors");
-
 
 app.use(cors());
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.json({ info: "Utilizar o /:codigoEmpcomenda para pesquisar" });
+});
+
+app.get("/rastreio/:codigo", async (req, res) => {
+  let codigoRastreio = req.params.codigo;
+  let { data } = await axios.get(`${url}/${codigoRastreio}`);
+  const $ = cheerio.load(data);
+  let historico = [];
+  $('div[class="singlepost"]').each(async (i, e) => {
+    let texto = $(e).children().text()
+    let teste = texto.split('<ul>')
+    teste.forEach(val => {
+      let resp = val.split('li')
+      resp.forEach(t => {
+        let filtrado = t.replaceAll(/<[^>]*(>|$)|&nbsp;|&zwnj;|&raquo;|&laquo;|&gt;/g, '').replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '').toString()
+        // let emArray = filtrado.split('Status')
+        String(filtrado).split('\n').forEach(este => {
+          este.split('Status ').forEach(outroTeste => {
+            outroTeste.split('\t').forEach(async v => {
+              if(v !== '') {
+                let obj = {};
+                if(v.includes('Objeto')) {
+                  obj.status = v
+                  if(v.includes('Unidade')) {
+                    obj.local = v
+                  }
+                  historico.push(obj);
+                }
+              }
+            })
+          })
+        })
+      })
+    })
+  })
+  res.json(historico)
 });
 
 app.get("/codigo/:codigo", async (req, res) => {
