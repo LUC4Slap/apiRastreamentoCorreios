adac38b8e082b6247167615e47f1c1f3ab6113b1
lucas__almeida <lucaslap27@gmail.com>
2021-09-09 07:33:52 -0400

    Adiciona api para rastrio

--- start.js
+++ start.js
@@ -4,6 +4,7 @@ const puppetter = require("puppeteer");
 const cheerio = require("cheerio");
 const axios = require("axios");
 const fs = require("fs");
+const rastrojs = require('rastrojs');
 
 let url = "https://www.linkcorreios.com.br";
 
@@ -19,37 +20,8 @@ app.get("/", (req, res) => {
 
 app.get("/rastreio/:codigo", async (req, res) => {
   let codigoRastreio = req.params.codigo;
-  let { data } = await axios.get(`${url}/${codigoRastreio}`);
-  const $ = cheerio.load(data);
-  let historico = [];
-  $('div[class="singlepost"]').each(async (i, e) => {
-    let texto = $(e).children().text()
-    let teste = texto.split('<ul>')
-    teste.forEach(val => {
-      let resp = val.split('li')
-      resp.forEach(t => {
-        let filtrado = t.replaceAll(/<[^>]*(>|$)|&nbsp;|&zwnj;|&raquo;|&laquo;|&gt;/g, '').replace(/[&\/\\#,+()$~%.'":*?<>{}]/g, '').toString()
-        // let emArray = filtrado.split('Status')
-        String(filtrado).split('\n').forEach(este => {
-          este.split('Status ').forEach(outroTeste => {
-            outroTeste.split('\t').forEach(async v => {
-              if(v !== '') {
-                let obj = {};
-                if(v.includes('Objeto')) {
-                  obj.status = v
-                  if(v.includes('Unidade')) {
-                    obj.local = v
-                  }
-                  historico.push(obj);
-                }
-              }
-            })
-          })
-        })
-      })
-    })
-  })
-  res.json(historico)
+  const tracks = await rastrojs.track(codigoRastreio, 'NOT-CODE');
+  res.json(tracks)
 });
 
 app.get("/codigo/:codigo", async (req, res) => {
